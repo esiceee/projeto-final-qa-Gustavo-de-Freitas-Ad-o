0a3a6f7868e0de9373201b102c0491a6386e4cd1
esiceee <132100164+esiceee@users.noreply.github.com>
2025-05-23 15:34:58 -0300

    Update Teste_03.py

--- Testes/Teste_03.py
+++ Testes/Teste_03.py
@@ -1,1 +1,20 @@
+!pip install requests
+!pip install beautifulsoup4
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.nasa.gov/"
+response = requests.get(url)
+
+if response.status_code == 200:
+
+  soup = BeautifulSoup(response.text, "html.parser")
+
+  links = soup.find_all('a')
+
+  for link in links:
+    print(link.get('href'))
+
+else:
+  print("Erro ao acessar a página:", response.status_code)
 
