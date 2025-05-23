c9103acf9e708920830a217552d57d605526bd21
esiceee <132100164+esiceee@users.noreply.github.com>
2025-05-23 14:57:08 -0300

    Update Teste_01.py

--- Testes/Teste_01.py
+++ Testes/Teste_01.py
@@ -1,1 +1,16 @@
+import unittest
 
+def somar(a, b):
+  return a + b
+class TestSomar(unittest.TestCase):
+    def test_somar_positivos(self):
+        self.assertEqual(somar(2, 3), 5)
+
+    def test_somar_negativos(self):
+        self.assertEqual(somar(-1, -1), -2)
+
+    def test_somar_zero(self):
+        self.assertEqual(somar(0, 0), 0)
+
+if __name__ == '__main__':
+    unittest.main(argv=[''], exit=False)
